dc4bae2c5bddc555005eb69e64a979765312c961
Hong-Sun2018 <sunhong_ra@live.cn>
2021-12-27 14:15:33 +0100

    installed material ui

--- layout/HeaderBar.js
+++ layout/HeaderBar.js
@@ -1,10 +1,19 @@
+import { makeStyles } from "@material-ui/core/styles";
 
+const useStyles = makeStyles({
+    root: {
+        margin: 0,
+        height: '3em',
+        background: 'yellow',
+    },
+});
 
 const HeaderBar = () => {
+    const classes = useStyles();
     return (
-        <>
+        <div className={classes.root}>
         <h1>Header Bar</h1>
-        </>
+        </div>
     );
 }
 
